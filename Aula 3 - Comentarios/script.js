624080a95005bb1eb7a1aecd6abc7c904fec9712
Joao Lino <linusmannuel@gmail.com>
2022-01-20 07:24:04 +0100

    feat: Aula 3 - Comentários

---
+++ Aula 3 - Comentarios/script.js
@@ -0,0 +1,6 @@
+//No JS podemos criar um comentário de linha ou em bloco.
+console.log('Aprendendo JS na prática');
+/*
+  Bloco de código...
+*/
+console.log('Os comentários serão ignorados pelo motor do JS V8 da Google');
