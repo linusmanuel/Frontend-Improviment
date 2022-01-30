803cbcc8890c9cde8a25cc272b9fbfa17d7ab5f9
Joao Lino <linusmannuel@gmail.com>
2022-01-30 06:02:10 +0100

    feat: Aula 6 - Variaveis

---
+++ Aula 6 - Variaveis/script.js
@@ -0,0 +1,14 @@
+/* 
+  1 - Não podemos criar variáveis com palavras reservadas;
+  2 - Variáveis precisam ter nomes significativos;
+  3 - Não pode começar o nome de uma variável com um número;
+  4 - Não podem conter espaços ou traços em nomes de variáveis;
+  5 - Utilizamos camelCase;
+  6 - Case-sensitive;
+  7 - Não podemos redeclarar variáveis com let;
+*/
+let nomeCliente = 'Luiz';
+nomeCliente = 'Otávio'; //Alterando o valor contido na variável nomeCliente;
+
+//Exibindo o valor da variável no console;
+console.log(nomeCliente);
