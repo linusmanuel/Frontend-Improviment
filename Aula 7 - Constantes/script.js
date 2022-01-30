73faa349ac7340af666a578d277bc9d81d97e039
Joao Lino <linusmannuel@gmail.com>
2022-01-30 06:28:52 +0100

    feat: Aula 7 -  Constantes

---
+++ Aula 7 - Constantes/script.js
@@ -0,0 +1,15 @@
+/* 
+  1 - Não podemos criar CONSTANTES com palavras reservadas;
+  2 - Variáveis precisam ter nomes significativos;
+  3 - Não pode começar o nome de uma variável com um número;
+  4 - Não podem conter espaços ou traços em nomes de CONSTANTES;
+  5 - Utilizamos camelCase;
+  6 - Case-sensitive;
+  7 - Não podemos redefinir o valor de uma CONSTANTE;
+*/
+const firstNumber = 10;
+const secondNumber = 40;
+const result = firstNumber + secondNumber;
+
+//Exibindo o resultado;
+console.log(`${firstNumber} + ${secondNumber} = ${result}`)
