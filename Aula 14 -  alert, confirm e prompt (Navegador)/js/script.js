b8fd0491c84bc78e24eb8f040b912f3588eb18cd
Joao Lino <linusmannuel@gmail.com>
2022-02-01 07:10:42 +0100

    feat: Deixando o código mais explicito

--- Aula 14 -  alert, confirm e prompt (Navegador)/js/script.js
+++ Aula 14 -  alert, confirm e prompt (Navegador)/js/script.js
@@ -3,7 +3,7 @@ num1 = prompt('Digite um numero: ');
 num2 = prompt('Digite outro numero: ');
 
 //Convertendo o tipo das variaveis 
-// parseInt(num1);
-// parseInt(num2);
+num1 = parseInt(num1);
+num2 = parseInt(num2);
 
-console.log(`O resultado de ${num1} + ${num2} é: ${Number(num1) + Number(num2)}`);
+alert(`O resultado de ${num1} + ${num2} é: ${num1 + num2}`);
