fae5bdc4cd9f8fb223bd06a8961e7016c9d87be5
Joao Lino <linusmannuel@gmail.com>
2022-02-08 17:50:56 +0100

    feat(Aula 17 Plus): Finalizando os exercicios

--- Aula 17 - Plus Exercicio/js/script.js
+++ Aula 17 - Plus Exercicio/js/script.js
@@ -6,13 +6,16 @@
   ""     lastIndexOf(); 
   ""     toLowerCase();
   ""     toUpperCase();
+  ""     slice();
   Operador mais como concatenador de valores;
 */
 const nome = prompt('Digite seu nome completo: ');
 document.body.innerHTML += `Seu nome é: ${nome}<br/>`;
 document.body.innerHTML += `Seu nome tem ${nome.length} letras <br/>`;
 document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br/>`;
-document.body.innerHTML += `Qual o primeiro índice da LETRA (A) do seu nome?: ${nome.indexOf('a')}`;
-
-
-
+document.body.innerHTML += `Qual o primeiro índice da LETRA (A) do seu nome?: ${nome.indexOf('a')} <br/>`;
+document.body.innerHTML += `Qual o último índice da letra (A) no seu nome? ${nome.lastIndexOf('a')}<br/>`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}? <br/>`;
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br/>`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br/>`;
+document.body.innerHTML += `Seu nome em letras minúsculas: ${nome.toLowerCase()}`;
