3db933d18149e761ccc2ad1cbb3ec7950638ed0c
Joao Lino <linusmannuel@gmail.com>
2022-02-18 15:00:02 +0100

    feat: Aula 19 - Mais sobre numeros

---
+++ Aula 19 - Mais sobre numeros/js/script.js
@@ -0,0 +1,10 @@
+let num1 = 128; // number;
+let num2 = 2.5; // number;
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+let temp = num1 * 'ola';
+
+console.log(Number.isNaN(temp));
